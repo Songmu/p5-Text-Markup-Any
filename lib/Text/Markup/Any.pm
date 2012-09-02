8c92797c43014694d6660d62c39fa2c70dcbf492
Masayuki Matsuki <y.songmu@gmail.com>
2012-09-02 18:31:27 +0900

    initial commit

---
+++ lib/Text/Markup/Any.pm
@@ -0,0 +1,32 @@
+package Text::Markup::Any;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Text::Markup::Any -
+
+=head1 SYNOPSIS
+
+  use Text::Markup::Any;
+
+=head1 DESCRIPTION
+
+Text::Markup::Any is
+
+=head1 AUTHOR
+
+Masayuki Matsuki E<lt>y.songmu@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
